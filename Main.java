bc07be15d92ca5a7f9e16142e7d20fabb053218d
juliaalvesap <alvesjulia072072@gmail.com>
2025-05-13 15:23:17 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class BancoApp {
+    public static void main(String[] args) {
+        
+        Conta minhaConta = new Conta(1500.50, 1000.00);
+        
+        minhaConta.adicionarCliente(new Cliente("Ana Pereira", "123.345.567-89"));
+        minhaConta.adicionarCliente(new Cliente("Carlos Santos", "987.765.543-21"));
+        minhaConta.adicionarCliente(null); // Cliente nulo
+        
+        minhaConta.mostrainfo();
+        
+        System.out.println("\nPrograma executado com sucesso!");
+    }
+}
